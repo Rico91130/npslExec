48db9263e1fe043f2048149722a8879a101bf02a
Rico91130 <76157511+Rico91130@users.noreply.github.com>
2025-12-22 18:00:22 +0100

    Update bookmark-1-inject.js

--- bookmark-1-inject.js
+++ bookmark-1-inject.js
@@ -2,8 +2,8 @@ javascript:(function(){
     const REPO = "https://raw.githubusercontent.com/Rico91130/npslExec/main/";
     const FILES = [
         { name: 'moteur.js', key: 'MON_MOTEUR_LIB' },
-        { name: 'dashboard.js', key: 'MON_DASHBOARD' }, // Nouveau !
-        { name: 'toolbar.js', key: 'MON_TOOLBAR' }      // Nouveau !
+        { name: 'dashboard.js', key: 'MON_DASHBOARD' },
+        { name: 'toolbar.js', key: 'MON_TOOLBAR' }
     ];
 
     Promise.all(FILES.map(f => fetch(REPO + f.name).then(r => r.text()).then(c => localStorage.setItem(f.key, c))))
