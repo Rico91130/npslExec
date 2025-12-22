cb0c0cc51f623e9872891fd3cb1b7c72a3c0cb3f
Rico91130 <76157511+Rico91130@users.noreply.github.com>
2025-12-22 17:58:41 +0100

    Update bookmark-2-execute.js

--- bookmark-2-execute.js
+++ bookmark-2-execute.js
@@ -1,9 +1,5 @@
 javascript:(function(){
-    const clientCode = localStorage.getItem('MON_MOTEUR_CLIENT');
-    if(clientCode) {
-        window.eval(clientCode);
-    } else {
-        alert("⚠️ Outils non installés. Lancez 'Update Moteur' depuis la page d'accueil.");
-        window.open('https://demarches.service-public.gouv.fr/', '_blank');
-    }
+    const code = localStorage.getItem('MON_TOOLBAR');
+    if(code) window.eval(code);
+    else alert("Toolbar non installée !");
 })();
